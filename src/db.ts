4838abe74cb30eb51895e6347fddb2866c2ab797
Villegas, Edgar <Edgar.Villegas@mckesson.com>
2023-08-15 22:55:38 -0600

    Modify address database

--- src/db.ts
+++ src/db.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import config from "./config";
 
 export default async function connectDB() {
-    const DB_URI = `mongodb://${config.mongo.username}:${config.mongo.password}@${config.server.host}:${config.mongo.port}`;
+    const DB_URI = `mongodb://${config.mongo.username}:${config.mongo.password}@${config.mongo.host}:${config.mongo.port}`;
     mongoose.connect(DB_URI)
     .then(() => console.log("DB is connected"))
     .catch(error => console.log(console.error('Error connecting to MongoDB:', error)));
