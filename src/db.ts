113846f189494ec3c1c216cea831bc121c74da18
Villegas, Edgar <Edgar.Villegas@mckesson.com>
2023-08-15 23:16:53 -0600

    Add production and develop environment mode

--- src/db.ts
+++ src/db.ts
@@ -2,7 +2,12 @@ import mongoose from "mongoose";
 import config from "./config";
 
 export default async function connectDB() {
-    const DB_URI = `mongodb://${config.mongo.username}:${config.mongo.password}@${config.mongo.host}:${config.mongo.port}`;
+    let DB_URI = '';
+    if(process.env.NODE_ENV === 'production') {
+        DB_URI = `mongodb+srv://${config.mongo.username}:${config.mongo.password}@${config.mongo.host}`;
+    } else {
+        DB_URI = `mongodb://${config.mongo.username}:${config.mongo.password}@${config.mongo.host}`;
+    }
     mongoose.connect(DB_URI)
     .then(() => console.log("DB is connected"))
     .catch(error => console.log(console.error('Error connecting to MongoDB:', error)));
